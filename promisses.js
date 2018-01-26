1926e1e7d3fa348173b83bcda344a2f49386d6e6
Diogo Paza <diogopazacvel@gmail.com>
2018-01-26 12:05:30 -0200

    settimeout

---
+++ promisses.js
@@ -0,0 +1,24 @@
+const divDelayed = (a, b) => new Promise((resolve, reject) => {
+    setTimeout( () => {
+
+        if( b == 0) reject('O valor de B não pode ser zero!')
+        
+        resolve( a / b);
+    }, 2000);
+})
+
+
+
+/*
+divDelayed(10,5)
+    .then(resp => console.log( 'Sucesso: ' + resp))
+    .catch(err => console.log(err));
+*/
+
+    setTimeout(() => {
+
+        data = new Date; 
+        console.log("apos 5 s" + data)
+
+    }, 5000)
+    
